7f8ab83eb9a56ca9c07e066cba84361436404d82
vaisvila <vaisvila2@gmail.com>
2025-03-14 10:25:43 -0700

    adding weekly threshold to loss figure

--- LossSummary.R
+++ LossSummary.R
@@ -36,29 +36,40 @@ wr_thresholds <- read_csv('CodeFiles/weeklyThresholds.csv') %>% #pulling in week
 #processing salmon loss table for WR
 temp <- salmon_raw %>%
   filter(AdClip == 'N' & (.[[8]] == 'W' | .[[9]] == 'W')) %>%
-  select(14,Size_Race = 8,DNA_Race = 9,13) %>%
-  mutate(confirmed = case_when(Size_Race == 'W' & DNA_Race != 'W' ~ 'NO',
-                               DNA_Race == 'W' ~ 'YES',
-                               Size_Race == 'W' & is.na(DNA_Race) ~ 'PARTIAL')) %>%
+  select(Date, Size_Race = 8, DNA_Race = 9, LOSS) %>%  # Ensure Date is included
+  mutate(confirmed = case_when(
+    Size_Race == 'W' & DNA_Race != 'W' ~ 'NO',
+    DNA_Race == 'W' ~ 'YES',
+    Size_Race == 'W' & is.na(DNA_Race) ~ 'PARTIAL'
+  )) %>%
   filter(confirmed != 'NO') %>%
   group_by(Date, confirmed) %>%
-  summarize(loss = sum(LOSS))
+  summarize(loss = sum(LOSS), .groups = 'drop')  # Adding .groups = 'drop' to avoid warnings
 
 wr_weekly <- data.frame(Date = seq(as.Date('2024-12-01'), as.Date('2025-06-30'), 1)) %>%
   left_join(temp, by = 'Date') %>%
   left_join(wr_thresholds, by = 'Date') %>%
   replace(is.na(.), 0) %>%
   mutate(threshold = round(threshold, 2)) %>%
-  mutate(sum_7D_loss = rollsum(loss, k = 7, fill = 'left', align = 'right')) %>%
-  filter(Date <= Sys.Date() & Date >= Sys.Date()-6) %>%
+  mutate(sum_7D_loss = rollsum(loss, k = 7, fill = NA, align = 'right')) %>%
+  filter(Date <= Sys.Date() & Date >= Sys.Date() - 6) %>%
   mutate(triggered = if_else(sum_7D_loss < threshold, 'No', 'Yes')) %>%
-  mutate(loss = if_else(confirmed == 'PARTIAL', paste0(as.character(loss),'*'), as.character(loss)))
+  mutate(loss = if_else(confirmed == 'PARTIAL', paste0(as.character(loss), '*'), as.character(loss)))
 
 wr_table <- wr_weekly %>%
   mutate(Date = format(Date, "%b %d")) %>%
   select(Date, 'Winter-run Daily Loss' = 3, 'Winter-run 7-day rolling sum loss' = 6, 
          'Winter-run Daily Threshold' = 5, 'Winter-run Daily Trigger' = 7)
 
+#this is for the entire water year up until this point
+wr_weekly_WY <- data.frame(Date = seq(as.Date('2024-12-01'), as.Date(Sys.Date()), 1)) %>%
+  left_join(temp, by = 'Date') %>%
+  left_join(wr_thresholds, by = 'Date') %>%
+  replace(is.na(.), 0) %>%
+  mutate(threshold = round(threshold, 2)) %>%
+  mutate(sum_7D_loss = rollsum(loss, k = 7, fill = NA, align = 'right')) %>%
+  mutate(triggered = if_else(sum_7D_loss < threshold, 'No', 'Yes')) %>%
+  mutate(loss = if_else(confirmed == 'PARTIAL', paste0(as.character(loss), '*'), as.character(loss)))
 
 ############steelhead weekly threshold
 salvage <- steelhead_raw %>% select(1,3,9) %>%
@@ -70,10 +81,9 @@ salvage <- steelhead_raw %>% select(1,3,9) %>%
 SH_weekly <- data.frame(Date = seq(as.Date('2024-12-01'), as.Date('2025-06-30'), 1)) %>%
   left_join(salvage, by = 'Date') %>%
   replace(is.na(.), 0) %>%
-  mutate(sum_7D_salvage = rollsum(salvage, k = 7, fill = 'left', align = 'right')) %>%
-  filter(Date <= Sys.Date() & Date >= Sys.Date()-6) %>%
+  mutate(sum_7D_salvage = rollsum(salvage, k = 7, fill = NA, align = 'right')) %>%
+  filter(Date <= Sys.Date() & Date >= Sys.Date() - 6) %>%
   mutate(triggered = if_else(sum_7D_salvage < 120, 'No', 'Yes'))
-
 
 thick_border <- fp_border(color = "black", width = 2)
 
@@ -95,6 +105,13 @@ weekly_table <- SH_weekly %>%
   set_caption(caption = 'Summary of weekly salvage of steelhead and loss of winter-run to inform weekly distributed loss thresholds. Steelhead thresholds are triggered when the 7-day rolling sum of expanded salvage exceeds 120 fish.  Winter-run thresholds are triggered when 7-day rolling sum of loss exceeds the daily threshold based on historic proportion of winter-run present in the delta.*All or a portion of Winter-run loss has not been genetically confirmed',
               align_with_table = FALSE)
 weekly_table
+
+#weekly steelhead thresholds for the entire water year
+SH_weekly_WY <- data.frame(Date = seq(as.Date('2024-12-01'), as.Date(Sys.Date()), 1)) %>%
+  left_join(salvage, by = 'Date') %>%
+  replace(is.na(.), 0) %>%
+  mutate(sum_7D_salvage = rollsum(salvage, k = 7, fill = NA, align = 'right')) %>%
+  mutate(triggered = if_else(sum_7D_salvage < 120, 'No', 'Yes'))
 
 ############cumulative loss of steelhead and winter-run
 temp_wr <- salmon_raw %>%
@@ -143,11 +160,11 @@ lossmax <- cumulative_loss %>% group_by(species) %>% summarize(Date = max(Date),
   mutate(threshold = round(threshold, 1))
 
 thresholds <- data.frame(species = c('Steelhead', 'Steelhead', 'Steelhead', 
-                                    'Winter-run', 'Winter-run', 'Winter-run'),
+                                     'Winter-run', 'Winter-run', 'Winter-run'),
                          threshold = c('perc100', 'perc75', 'perc50',
                                        'perc100', 'perc75', 'perc50'),
-                        thresholds = c(3000, 3000*.75, 3000*.5, 
-                                      jpe*0.005, jpe*0.005*.75, jpe*0.005*.5)) %>%
+                         thresholds = c(3000, 3000*.75, 3000*.5, 
+                                        jpe*0.005, jpe*0.005*.75, jpe*0.005*.5)) %>%
   mutate(thresholds = round(thresholds, 0)) %>%
   pivot_wider(names_from = threshold, values_from = thresholds) %>%
   mutate(label = paste0('Thresholds:\n',perc100, ' (100%)\n',
@@ -156,31 +173,55 @@ thresholds <- data.frame(species = c('Steelhead', 'Steelhead', 'Steelhead',
   left_join(select(lossmax, species,cumul_loss), by = 'species')
 
 #annual_thresholds <- expand.grid(threshold_perc = c(.5, .75, 1), species = c('steelhead', 'winter-run')) %>%
-  #mutate(threshold = if_else(species == 'steelhead', 3000 * threshold_perc, jpe*.005*threshold_perc))
+#mutate(threshold = if_else(species == 'steelhead', 3000 * threshold_perc, jpe*.005*threshold_perc))
+
+####need to bind salmon and steelhead together to play nicely with figure code as is
+# Add a species column to each data frame
+SH_weekly_WY <- SH_weekly_WY %>%
+  mutate(species = "Steelhead") %>%
+  rename(sum_7D = sum_7D_salvage)
+
+wr_weekly_WY <- wr_weekly_WY %>%
+  mutate(species = "Winter-run") %>%
+  rename(sum_7D = sum_7D_loss)
+
+# Combine both data frames
+combined_weekly <- bind_rows(SH_weekly_WY, wr_weekly_WY)
+
+# Create a horizontal line data frame for Steelhead
+hline_data <- data.frame(yintercept = 120, species = "Steelhead")
 
 loss_graph <- ggplot(cumulative_loss) +
   geom_line(aes(x = Date, y = cumul_loss, color = "Cumulative Loss"), linetype = 'dashed', linewidth = 1) +
   geom_col(aes(x = Date, y = loss, fill = facility), position = 'dodge') +
-  geom_text(lossmax, mapping = aes(x = Date + 1, y = cumul_loss*1.05, 
-                                    label = paste0(cumul_loss,' (',threshold,'%)')), fontface = 'bold', size = 4) +
-  geom_text(data = thresholds, aes(x = min(cumulative_loss$Date)-15, 
-                                   y = cumul_loss*.90, 
+  geom_text(lossmax, mapping = aes(x = Date + 1, y = cumul_loss * 1.05, 
+                                   label = paste0(cumul_loss, ' (', threshold, '%)')), 
+            fontface = 'bold', size = 4) +
+  geom_text(data = thresholds, aes(x = min(cumulative_loss$Date) - 15, 
+                                   y = cumul_loss * .90, 
                                    label = label), 
             hjust = 0, fontface = "italic", size = 4) + 
+  geom_line(data = combined_weekly, aes(x = Date, y = sum_7D, color = "7 d Rolling Sum"), alpha = 0.5, size = 1) +  # Add lines for SH and WR data
+  geom_line(data = combined_weekly, aes(x = Date, y = threshold, color = "Weekly Distributed Loss Threshold"), linetype = "dotted", size = 1) + #winter-run threshold
+  # Add a condition to only show the line on the Steelhead facet
+  geom_hline(data = hline_data, aes(yintercept = yintercept), 
+             color = "red", linetype = "dotted", size = 1) +
   facet_wrap(~species, scales = 'free_y') +
-  scale_color_manual(name = "", values = 'black') +
+  scale_color_manual(name = "", values = c('black', 'blue', 'red')) +  # Adjust colors as needed
   scale_x_date(date_breaks = '6 weeks', date_labels = "%b %d", limits = c(as.Date('2024-12-01'), Sys.Date() + 14)) +
-  guides(color = guide_legend(override.aes = list(linetype = 'dashed'))) +
-  labs(y = 'Loss', fill = 'Facility') +
+  guides(color = guide_legend(override.aes = list(linetype = c('solid', 'dashed', 'dotted')))) +
+  labs(y = 'Loss', fill = 'Facility', x = NULL) +
   theme_bw() +
-  theme(plot.margin = margin(0.2,0.2,0.2,0.2, unit = 'cm'),
-        axis.title.y = element_text(margin=margin(r=15), size = 15),
-        axis.title.x = element_text(margin=margin(t=15), size = 15),
+  theme(plot.margin = margin(0.2, 0.5, 0.2, 0.2, unit = 'cm'),
+        axis.title.y = element_text(margin = margin(r = 15), size = 15),
+        axis.title.x = element_text(margin = margin(t = 15), size = 15),
         strip.text = element_text(size = 13),
         axis.text.x = element_text(size = 13),
         axis.text.y = element_text(size = 13),
         legend.position = 'bottom')
-loss_graph
+
+# Display the graph
+print(loss_graph)
 
 ggsave(plot = loss_graph, file = 'outputs/loss_summary.png', width = 9, height = 6)
 save_as_docx(weekly_table, loss_graph, path = paste0('outputs/salvage-summary_',Sys.Date(),'.docx'))
