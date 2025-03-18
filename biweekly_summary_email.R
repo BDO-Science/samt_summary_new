1d109b0525ab04235ef4ea2b67fba80ad4d3d365
caehlo <chase.ehlo@gmail.com>
2025-03-18 15:27:54 -0700

    email code updates

--- biweekly_summary_email.R
+++ biweekly_summary_email.R
@@ -15,6 +15,7 @@ partial <- temp %>% filter(confirmed == 'PARTIAL') %>% summarize(sum(loss)) %>% 
 SHloss <- cumulative_loss %>% filter(species == 'Steelhead') %>% summarize(maxloss = max(cumul_loss)) %>% pull()
 WRtriggers <- sum(wr_weekly$triggered == "Yes")
 SHtriggers <- sum(SH_weekly$triggered == "Yes")
+WR_hatch_loss <- max(wr_hatch$cumul_loss)
 
 # Create a new Outlook application object
 outlook_app <- COMCreate("Outlook.Application")
@@ -23,7 +24,7 @@ outlook_app <- COMCreate("Outlook.Application")
 email <- outlook_app$CreateItem(0)  # 0 corresponds to olMailItem
 
 # Set email properties
-email[["To"]] <- "jaisrael@usbr.gov; cehlo@usbr.gov; avaisvil@usbr.gov; lejohnson@usbr.gov; lejohnson@usbr.gov; mmanzo@usbr.gov; dmmooney@usbr.gov; twashburn@usbr.gov"  # Change to the recipient's email address
+email[["To"]] <- "jaisrael@usbr.gov; cehlo@usbr.gov; avaisvil@usbr.gov; bmahardja@usbr.gov; lejohnson@usbr.gov; mmanzo@usbr.gov; dmmooney@usbr.gov; twashburn@usbr.gov"  # Change to the recipient's email address
 email[["Subject"]] <- paste0("Salmonid Loss as of ", format(Sys.Date(), "%B %d, %Y"))
 
 img_path <- file.path(tempdir(), "Loss_Table.png")
@@ -48,13 +49,18 @@ img_base64 <- base64enc::dataURI(file = img_path, mime = "image/png")
 email_body <- paste0(
   "<p>Hi all,</p>",
   "<p>Please see the summary of the most recent loss estimates at salvage facilities.</p>",
-  "<p>Total annual loss of natural Winter-run Chinook Salmon as of ", 
+  "<ul>",
+  "<li>Total annual loss of natural Winter-run Chinook Salmon as of ", 
   format(Sys.Date() - 1, "%B %d, %Y"), 
-  " is <strong>", WRloss - partial, "</strong>, and loss of unconfirmed Winter-run Chinook Salmon is <strong>", partial, "</strong>.</p>",
-  "<p>Total annual loss of CCV steelhead as of ", 
+  " is <strong>", WRloss - partial, "</strong>, and loss of unconfirmed Winter-run Chinook Salmon is <strong>", partial, "</strong>.</li>",
+  "<li>Total annual loss of hatchery Winter-run Chinook Salmon as of ", 
   format(Sys.Date() - 1, "%B %d, %Y"), 
-  " is <strong>", SHloss, "</strong>.</p>",
-  "<p>There have been <strong>", WRtriggers, "</strong> triggers of the Winter-run and <strong>", SHtriggers, "</strong> triggers of the steelhead distributed weekly loss thresholds in the past week.</p>",
+  " is <strong>", WR_hatch_loss, "</strong>.</li>",
+  "<li>Total annual loss of CCV steelhead as of ", 
+  format(Sys.Date() - 1, "%B %d, %Y"), 
+  " is <strong>", SHloss, "</strong>.</li>",
+  "<li>There have been <strong>", WRtriggers, "</strong> triggers of the Winter-run and <strong>", SHtriggers, "</strong> triggers of the steelhead distributed weekly loss thresholds in the past week.</li>",
+  "</ul>",
   "<p><img src='", img_base64, "' alt='Loss Graph' style='width: 100%; max-width: 800px;'/></p>",
   "<p>Best regards,<br>SaMT Team</p>" 
 )
